f74941df8a18e7629f353eb19f9d9a1f3ad8ad65
Mttriple <mtemptriple@gmail.com>
2024-05-15 01:17:18 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f7127cda-28c6-46f0-b772-59acabc9773e.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
